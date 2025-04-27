6d0e028f8202be7be7d5a8e69d8d03ba5cf7135f
Neo <surendarraja420@gmail.com>
2025-04-27 13:14:17 +0530

    fiix: motion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,6 +15,9 @@ const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
       <motion.h1
+      whileInView={{opacity:1,y:0}}
+      initial={{opacity:0,y:-100}}
+      transition={{duration:1.5}}
       className="my-20 text-center text-4xl">About 
         <span className="text-neutral-500"> Me</span></motion.h1>
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
                      variants={container(0)}
                      initial="hidden"
                      animate="visible"
-                    className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+                    className="pb-14 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
                         Neo Strider</motion.h1>
                         <motion.span 
                         variants={container(0.5)}
